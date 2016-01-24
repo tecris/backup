f6fc160774aba1c78c99bace87fd38e6eb40796c
Eugen Cristea <eugen@cristea.org>
2016-01-24 11:22:01 +0100

    update

--- configureBitbucket.sh
+++ configureBitbucket.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# https://deanclatworthy.com/2013/01/how-to-avoid-relying-on-github-mirror-your-repository/
+
+
 REPO_NAME=`git remote -v|grep fetch|awk -F'\/' '{print $2}'|grep fetch|awk -F'.' '{print $1}'`
 
 echo Configure $REPO_NAME
@@ -10,7 +13,6 @@ git remote add bitbucket ssh://git@bitbucket.org/tecris/$REPO_NAME.git
 
 GIT_CONFIG=.git/config
 
-echo "" >> $GIT_CONFIG
 echo [remote \"origin\"] >> $GIT_CONFIG
-echo "    url = git@github.com:tecris/$REPO_NAME.git" >> $GIT_CONFIG
-echo "    url = ssh://git@bitbucket.org/tecris/$REPO_NAME.git" >> $GIT_CONFIG
+echo "        url = git@github.com:tecris/$REPO_NAME.git" >> $GIT_CONFIG
+echo "        url = ssh://git@bitbucket.org/tecris/$REPO_NAME.git" >> $GIT_CONFIG
