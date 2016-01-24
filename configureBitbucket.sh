ac12e1cd628ae82bf33e676c432ebae77d578635
Eugen Cristea <eugen@cristea.org>
2016-01-24 10:47:43 +0100

    update

--- configureBitbucket.sh
+++ configureBitbucket.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO_NAME=`git remote -v|grep fetch|awk -F'\/' '{print $2}'`
+REPO_NAME=`git remote -v|grep fetch|awk -F'\/' '{print $2}'|grep fetch|awk -F'.' '{print $1}'`
 
 echo $REPO_NAME
 
