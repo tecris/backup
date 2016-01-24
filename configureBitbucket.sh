98ca0f2114442fdf909252e970da02374d4b3b1d
Eugen Cristea <eugen@cristea.org>
2016-01-24 11:15:51 +0100

    update

--- configureBitbucket.sh
+++ configureBitbucket.sh
@@ -2,5 +2,15 @@
 
 REPO_NAME=`git remote -v|grep fetch|awk -F'\/' '{print $2}'|grep fetch|awk -F'.' '{print $1}'`
 
-echo $REPO_NAME
+echo Configure $REPO_NAME
 
+git remote rename origin github
+
+git remote add bitbucket ssh://git@bitbucket.org/tecris/$REPO_NAME.git
+
+GIT_CONFIG=.git/config
+
+echo "" >> $GIT_CONFIG
+echo [remote "origin"] >> $GIT_CONFIG
+echo "    url = git@github.com:tecris/$REPO_NAME.git" >> $GIT_CONFIG
+echo "    url = ssh://git@bitbucket.org/tecris/$REPO_NAME.git" >> $GIT_CONFIG
